1aedd591010da535985621be4ddddb5592da807b
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-07-26 09:33:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.cpp
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -1,56 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to find the majority elements in the array
     vector<int> findMajority(vector<int>& arr) {
-        // Your code goes here.
-        int n=arr.size();
-        int val=n/3;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
+        // Code here
+        unordered_map<int, int> mp;
+        for (int& it : arr){
+            mp[it]++;
         }
-        vector<int>ans;
-        for(auto &pr:mp){
-            if(pr.second>val){
-                ans.push_back(pr.first);
-            }
+        int n = arr.size();
+        vector<int> ans;
+        for (auto& it : mp){
+            if (it.second > n/3) ans.push_back(it.first);
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        string s;
-        getline(cin, s);
-        stringstream ss(s);
-        vector<int> nums;
-        int num;
-        while (ss >> num) {
-            nums.push_back(num);
-        }
-        Solution ob;
-        vector<int> ans = ob.findMajority(nums);
-        if (ans.empty()) {
-            cout << "[]";
-        } else {
-            for (auto &i : ans)
-                cout << i << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
