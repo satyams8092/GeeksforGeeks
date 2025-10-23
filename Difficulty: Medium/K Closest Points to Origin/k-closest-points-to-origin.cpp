e0afe71dd3df78ba89a11a2276cd7b4e2c495606
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-10-23 22:16:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -1,65 +1,31 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        // Your code here
-        priority_queue<pair<int,int>> pq; //max-heap
+        // code here
+          priority_queue<pair<double,pair<int,int>>>pq;
         
-        int n=points.size();
-        
-        for(int i=0;i<n;i++){
-            int x=points[i][0];
-            int y=points[i][1];
-            int d = x*x + y*y;
+        for(auto p:points){
+            int x=p[0];
+            int y=p[1];
             
-            pq.push({d,i});
-            
-            if(pq.size()>k)
-            pq.pop();
+             double d=(double)sqrt(x*x+y*y);
+             
+             pq.push({d,{x,y}});
+             
+             if(pq.size()>k){
+                 pq.pop();
+             }
         }
         
-        vector<vector<int>> res;
+        
+        
+        vector<vector<int>>res;
         
         while(!pq.empty()){
-            int i = pq.top().second;
+            res.push_back({pq.top().second.first,pq.top().second.second});
             pq.pop();
-            res.push_back({points[i][0],points[i][1]});
         }
         
         return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int k;
-        cin >> k;
-        int n;
-        cin >> n;
-        vector<vector<int>> points(n, vector<int>(2));
-        for (int i = 0; i < n; i++) {
-            cin >> points[i][0] >> points[i][1];
-        }
-        Solution ob;
-        vector<vector<int>> ans = ob.kClosest(points, k);
-        sort(ans.begin(), ans.end());
-        for (const vector<int>& point : ans) {
-            cout << point[0] << " " << point[1] << endl;
-        }
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
