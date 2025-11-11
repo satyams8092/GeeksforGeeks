72b84fca14b325af5fadd4ee4ff7c5a3c130e342
SATYAM SHAH <143245535+satyams8092@users.noreply.github.com>
2025-11-11 09:33:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        // code here
+
+        int n = s1.size(), m = s2.size();
+
+        
+
+        // DP to find LCS
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= m; j++){
+
+                if(s1[i-1] == s2[j-1])
+
+                    dp[i][j] = 1 + dp[i-1][j-1];
+
+                else
+
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+
+            }
+
+        }
+
+        
+
+        int lcs = dp[n][m];
+
+        return n + m - lcs;
+    }
+};
